4ad8191d95824bc8a0b4f0041c636c1fae87adc1
Your Name <fonkouwilfried553@gmail.com>
2024-10-07 14:18:01 +0100

    Initial commit

---
+++ context/AppContext.js
@@ -0,0 +1,21 @@
+// File: context/AppContext.js
+
+import React, { createContext, useState } from 'react';
+
+export const AppContext = createContext();
+
+export const AppProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+
+  return (
+    <AppContext.Provider
+      value={{
+        user,
+        setUser,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
+
